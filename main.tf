80878b8244374a5143f047916ff8405bc021134f
Priyaanvi <rs4917112@gmail.com>
2026-07-16 00:08:07 +0530

    add rg

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.80.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "rg1234" {
+name = "rg111"
+location = "centralindia"
+}
